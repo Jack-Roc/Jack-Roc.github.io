816ea06e948035a33ce474ea46722c91a05afc8c
Jack-Roc <Jack-Roc@gmail.com>
2020-01-11 20:25:49 +0800

    clean all file

--- js/color.js
+++
@@ -1,6 +0,0 @@
-function randColor(){//随机颜色
-        var r = Math.floor(Math.random() * 256);
-        var g = Math.floor(Math.random() * 256);
-        var b = Math.floor(Math.random() * 256);
-        return "rgb("+r+","+g+","+b+")";
-    }
